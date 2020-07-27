b06ae05b6f78f9bbad00d440efe636d1444dd4af
Asif Alli <alli.asif@gmail.com>
2020-07-26 20:34:14 -0400

    much better soln

--- SomeApp/src/com/alliconsulting/practice/app/SherlockValidString.java
+++ SomeApp/src/com/alliconsulting/practice/app/SherlockValidString.java
@@ -11,48 +11,44 @@ public class SherlockValidString {
 		int[] freqs = new int[26];
 		for(char c:testString.toCharArray()) {
 			++freqs[((int)c)-'a'];
-		}		
-//		int oneCount=0;
-//		int repeatOccurrences=0;
-//		int thisFreq=0;
-//		int nextFreq=0;
+		}
+		
 		int mode = findMode(freqs);				
 		System.out.println( String.format("Mode of %s is %d",testString,mode));
 		
+		int numberOfModesFound = 0;
 		int spotsOccupiedByNumbers = 0;
-		for( int i=0;i<freqs.length;i++) {
-			if(freqs[i]>0) spotsOccupiedByNumbers++;
-		}
-		System.out.println( String.format("Spots occupied by numbers in %s is %d",testString,spotsOccupiedByNumbers));
-		
 		int numberOfFrequenciesOfOne = 0;
-		for( int i=0;i<freqs.length;i++) {
-			if(freqs[i]==1) numberOfFrequenciesOfOne++;
-		}		
-		System.out.println( String.format("Numbers of 1-frequencies in %s is %d",testString,numberOfFrequenciesOfOne));
-
 		int numberOfFrequenciesOfOddOccurrences = 0;
-		for( int i=0;i<freqs.length;i++) {
-			if(freqs[i]%2==1) numberOfFrequenciesOfOddOccurrences++;
-		}		
-		System.out.println( String.format("Numbers of Odd-frequencies in %s is %d",testString,numberOfFrequenciesOfOddOccurrences));
-		
 		int freqOfOddOccurrence = 0;
+		for( int i=0;i<freqs.length;i++) {
+			if(freqs[i]==mode) numberOfModesFound++;
+			if(freqs[i]>0) spotsOccupiedByNumbers++;
+			if(freqs[i]==1) numberOfFrequenciesOfOne++;
+			if(freqs[i]%2==1) numberOfFrequenciesOfOddOccurrences++;			
+		}
+		System.out.println( String.format("Numbers of modes found in %s is %d",testString,numberOfModesFound));
+		System.out.println( String.format("Spots occupied by numbers in %s is %d",testString,spotsOccupiedByNumbers));
+		System.out.println( String.format("Numbers of 1-frequencies in %s is %d",testString,numberOfFrequenciesOfOne));
+		System.out.println( String.format("Numbers of Odd-frequencies in %s is %d",testString,numberOfFrequenciesOfOddOccurrences));
+
 		if(numberOfFrequenciesOfOddOccurrences==1) {
 			for( int i=0;i<freqs.length;i++) {
 				if(freqs[i]%2==1) freqOfOddOccurrence=freqs[i];
 			}
 			System.out.println( String.format("Frequency value of the only Odd-frequency in %s is %d",testString,freqOfOddOccurrence));
 		}		
-		
-		int numberOfModesFound = 0;
-		for( int i=0;i<freqs.length;i++) {
-			if(freqs[i]==mode) numberOfModesFound++;
-		}
-		System.out.println( String.format("Numbers of modes found in %s is %d",testString,numberOfModesFound));
 		System.out.println();
 		System.out.println("---------------------------------");
-		return (numberOfModesFound==spotsOccupiedByNumbers) ? "YES" : ((numberOfModesFound==spotsOccupiedByNumbers-1 && numberOfFrequenciesOfOne==1) ? "YES" : ((numberOfModesFound==spotsOccupiedByNumbers-1 && numberOfFrequenciesOfOddOccurrences==1) ? "YES" : "NO"));
+		
+		if(numberOfModesFound==spotsOccupiedByNumbers)
+			return "YES";
+		else if (numberOfModesFound==spotsOccupiedByNumbers-1 && numberOfFrequenciesOfOne==1)
+			return "YES";
+		else if(numberOfModesFound==spotsOccupiedByNumbers-1 && numberOfFrequenciesOfOddOccurrences==1 && freqOfOddOccurrence==mode+1)
+			return "YES";
+		
+		return "NO";
 	}
 	
 	public int findMode(int[] originalArray)
