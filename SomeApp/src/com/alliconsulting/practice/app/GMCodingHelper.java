b1fda4faa64cbaa94a89e61978af5895d7df9cd1
Asif Alli <alli.asif@gmail.com>
2020-08-01 21:48:56 -0400

    gm code

--- SomeApp/src/com/alliconsulting/practice/app/GMCodingHelper.java
+++ SomeApp/src/com/alliconsulting/practice/app/GMCodingHelper.java
@@ -26,19 +26,7 @@ public class GMCodingHelper {
 		String result = "";
 
 		String amounts[] = line.split(";");
-		HashMap<String,Double> cashReg = new HashMap<String,Double>();
-		cashReg.put("PENNY", 0.01);
-		cashReg.put("NICKEL", 0.05);
-		cashReg.put("DIME", 0.10);
-		cashReg.put("QUARTER", 0.25);
-		cashReg.put("HALF DOLLAR", 0.50);
-		cashReg.put("ONE", 1.00);
-		cashReg.put("TWO", 2.00);
-		cashReg.put("FIVE", 5.00);
-		cashReg.put("TEN", 10.00);
-		cashReg.put("TWENTY", 20.00);
-		cashReg.put("FIFTY", 50.00);
-		cashReg.put("ONE HUNDRED", 100.00);
+
 		
 		BigDecimal pp = new BigDecimal(Double.valueOf(amounts[0])).setScale(2, RoundingMode.HALF_EVEN);
 		BigDecimal ch = new BigDecimal(Double.valueOf(amounts[1])).setScale(2, RoundingMode.HALF_EVEN);
@@ -63,6 +51,57 @@ public class GMCodingHelper {
 		
 		System.out.println(result);
 		return result;
+	}
+	
+	
+	
+	public int[] getCoins(double someNumber, HashMap<String,String> cashReg) {
+		int[] result = new int[] {};
+		
+		int i=1;
+		double remainder=someNumber;
+		while(remainder>=0) {
+			remainder-=largestCoin(remainder,cashReg);
+		}
+		
+		
+		return result;
+	}
+	
+	public double largestCoin(double someNumber,HashMap<String,String> cashReg) {
+		double returnCoin=0.0;
+		
+		for( String thisCoinValue:cashReg.keySet() ) {
+			BigDecimal thisCV = new BigDecimal(Double.valueOf(thisCoinValue)).setScale(2, RoundingMode.HALF_EVEN);
+			if( thisCV.doubleValue()<=someNumber) {
+				BigDecimal remainderBD=new BigDecimal(someNumber-thisCV.doubleValue()).setScale(2, RoundingMode.HALF_EVEN);;
+				cashReg.put(thisCoinValue, cashReg.get(thisCoinValue)+";"+remainderBD);
+			}
+			System.out.println(String.format("%s %s", thisCV,cashReg.get(thisCoinValue)));			
+		}
+		
+		double smallestRemainder = 1;
+		for( String thisCoinValue:cashReg.keySet() ) {
+			String thisValue = cashReg.get(thisCoinValue);			
+			double thisRemainder = (thisValue.split(";").length==3) ? new BigDecimal(thisValue.split(";")[2]).setScale(2, RoundingMode.HALF_EVEN).doubleValue() : 1;
+			if(thisRemainder<smallestRemainder)
+				smallestRemainder = thisRemainder;
+		}
+		
+		double largestCoin = 0.0;
+		for( String thisCoinValue:cashReg.keySet()) {
+			String thisValue = cashReg.get(thisCoinValue);
+			if(thisValue.split(";").length==3) {
+				double thisRemainder = new BigDecimal(thisValue.split(";")[2]).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
+				if(smallestRemainder==thisRemainder) {
+					largestCoin=new BigDecimal(thisCoinValue).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
+					cashReg.put(thisCoinValue, cashReg.get(thisCoinValue)+";"+remainderBD);
+					break;
+				}					
+			}
+		}
+		System.out.println(largestCoin);
+		return largestCoin;
 	}
 	
 }
--- SomeApp/test/com/alliconsulting/practice/tests/GMCodingTests.java
+++ SomeApp/test/com/alliconsulting/practice/tests/GMCodingTests.java
@@ -28,10 +28,25 @@ class GMCodingTests {
 	@Test
 	void test_change() {
 		String line = "15.94;16.00";
+		HashMap<String,String> cashReg = new HashMap<String,String>();
+		cashReg.put(""+0.01,"1;PENNY");
+		cashReg.put(""+0.05,"1;NICKEL");
+		cashReg.put(""+0.10,"1;DIME");
+		cashReg.put(""+0.25,"1;QUARTER");
+		cashReg.put(""+0.50,"1;HALF DOLLAR");
+		cashReg.put(""+1.00,"1;ONE");
+		cashReg.put(""+2.00,"1;TWO");
+		cashReg.put(""+5.00,"1;FIVE");
+		cashReg.put(""+10.00,"1;TEN");
+		cashReg.put(""+20.00,"1;TWENTY");
+		cashReg.put(""+50.00,"1;FIFTY");
+		cashReg.put(""+100.00,"1;ONE HUNDRED");		
 		//String line = "17;16.00";
 		//String line = "35;35";
 		
-		Assert.assertEquals("NICKEL,PENNY",gch.getChange(line));
+		//Assert.assertEquals("NICKEL,PENNY",gch.getChange(line));
+		//int[] coins = gch.getCoins(0.06, cashReg);
+		double rem = gch.largestCoin(0.06, cashReg);
 		
 	}
 
