5488ae5d29273eb7f48e901015d05f3a8a36eb12
Asif Alli <alli.asif@gmail.com>
2020-08-04 19:59:30 -0400

    solved for some tests

--- SomeApp/src/com/alliconsulting/practice/app/BribeHelper.java
+++ SomeApp/src/com/alliconsulting/practice/app/BribeHelper.java
@@ -7,14 +7,14 @@ public class BribeHelper {
 		int index = 0;
 		for(int i=0;i<q.length;i++) {
 			index = i;
-			if(isOutOfPlace(q[i],i)) {
-				if(numBribesForThis(q[i],i)>2) {
+			if(isOutOfPlace(q[i],i) && ( (i<q.length-1) ? q[i]>q[i+1] : true )) {
+				if(numBribesForThis(q[i],i,q)>2) {
 					System.out.println("Too chaotic");					
 					break;
 				}
-				totalBribes+=numBribesForThis(q[i],i);
+				totalBribes+=numBribesForThis(q[i],i,q);
 			}
-			System.out.println( String.format("index:%d, number:%d, bribes:%d", i,q[i], (q[i]>i) ? numBribesForThis(q[i],i) : 0));
+			System.out.println( String.format("index:%d, number:%d, bribes:%d", i,q[i], (q[i]>i) ? numBribesForThis(q[i],i,q) : 0));
 		}
 		if(index==q.length-1)
 			System.out.println(""+totalBribes);
@@ -24,7 +24,8 @@ public class BribeHelper {
 		return n!=position+1;
 	}
 	
-	public int numBribesForThis(int n, int currentIndex) {
+	public int numBribesForThis(int n, int currentIndex, int[] q) {
 		return (n-currentIndex>=1) ? Math.abs(currentIndex-(n-1)) : 0;
+		//return Math.abs(currentIndex-(n-1));
 	}
 }
