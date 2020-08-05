18c6507e33a138907d7f9e91e0bd6a099b2ca6dc
Asif <alli.asif@gmail.com>
2020-08-05 17:17:50 -0400

    solved

--- SomeApp/src/com/alliconsulting/practice/app/BribeHelper.java
+++ SomeApp/src/com/alliconsulting/practice/app/BribeHelper.java
@@ -7,25 +7,28 @@ public class BribeHelper {
 		int index = 0;
 		for(int i=0;i<q.length;i++) {
 			index = i;
-			if(isOutOfPlace(q[i],i) && ( (i<q.length-1) ? q[i]>q[i+1] : true )) {
+			if(isOutOfPlace(q[i],i,q)) {
 				if(numBribesForThis(q[i],i,q)>2) {
 					System.out.println("Too chaotic");					
 					break;
 				}
 				totalBribes+=numBribesForThis(q[i],i,q);
 			}
-			System.out.println( String.format("index:%d, number:%d, bribes:%d", i,q[i], (q[i]>i) ? numBribesForThis(q[i],i,q) : 0));
+			//System.out.println( String.format("index:%d, number:%d, bribes:%d", i,q[i], (isOutOfPlace(q[i],i,q)) ? numBribesForThis(q[i],i,q) : 0 ));
 		}
 		if(index==q.length-1)
 			System.out.println(""+totalBribes);
 	}
 	
-	public boolean isOutOfPlace(int n, int position) {
-		return n!=position+1;
+	public boolean isOutOfPlace(int n, int position, int[] queue) {
+		if( position<(queue.length-1) && n > (queue[position+1]) )
+			return (true || queue[position]>queue[position+1] );
+		if( position<(queue.length-1) && n > (position+1) )
+			return (true || queue[position]>queue[position+1] );
+		return false;
 	}
 	
 	public int numBribesForThis(int n, int currentIndex, int[] q) {
-		return (n-currentIndex>=1) ? Math.abs(currentIndex-(n-1)) : 0;
-		//return Math.abs(currentIndex-(n-1));
+		return Math.abs(currentIndex-(n-1));
 	}
 }
--- SomeApp/test/com/alliconsulting/practice/tests/BribeHelperTests.java
+++ SomeApp/test/com/alliconsulting/practice/tests/BribeHelperTests.java
@@ -1,5 +1,6 @@
 package com.alliconsulting.practice.tests;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import com.alliconsulting.practice.app.BribeHelper;
@@ -8,30 +9,28 @@ class BribeHelperTests {
 
 	BribeHelper bh = new BribeHelper();
 	
-	//@Test
+	@Test
 	void test() {
 		int[] queue = {2,1,5,3,4};
 		bh.minimumBribes(queue);
 	}
 	
-	//@Test
+	@Test
 	void test2() {
 		int[] queue = {2,5,1,3,4};
 		bh.minimumBribes(queue);
 	}
 	
-	//@Test
+	@Test
 	void test3() {
 		int[] queue = {5,1,2,3,7,8,6,4};
 		bh.minimumBribes(queue);
-		//Too
 	}
 	
 	@Test
 	void test4() {
 		int[] queue = {1,2,5,3,7,8,6,4};
 		bh.minimumBribes(queue);
-		//7
 	}
 	
 	//5 1 2 3 7 8 6 4
