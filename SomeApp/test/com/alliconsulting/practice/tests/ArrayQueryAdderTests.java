4e6b25b3a86c08b54a91b1d1880c817b53adca59
Asif Alli <alli.asif@gmail.com>
2020-08-07 01:26:23 -0400

    new

---
+++ SomeApp/test/com/alliconsulting/practice/tests/ArrayQueryAdderTests.java
@@ -0,0 +1,18 @@
+package com.alliconsulting.practice.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.alliconsulting.practice.app.ArrayQueryAdder;
+
+class ArrayQueryAdderTests {
+
+	ArrayQueryAdder aqa = new ArrayQueryAdder();
+	
+	@Test
+	void test() {
+		
+	}
+
+}
