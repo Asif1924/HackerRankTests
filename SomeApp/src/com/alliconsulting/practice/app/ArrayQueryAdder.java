5cef883fca76ba7ea7deb9c0cd84bec75cb7da49
Asif <alli.asif@gmail.com>
2020-08-10 01:34:17 -0400

    ddf

--- SomeApp/src/com/alliconsulting/practice/app/ArrayQueryAdder.java
+++ SomeApp/src/com/alliconsulting/practice/app/ArrayQueryAdder.java
@@ -1,5 +1,58 @@
 package com.alliconsulting.practice.app;
 
+import java.util.HashMap;
+
 public class ArrayQueryAdder {
+	/*
+	 *  a b k
+    	1 5 3
+    	4 8 7
+    	6 9 1
+    	
+    	
+index->	 1 2 3  4  5 6 7 8 9 10
+		[0,0,0, 0, 0,0,0,0,0, 0]
+		[3,3,3, 3, 3,0,0,0,0, 0]
+		[3,3,3,10,10,7,7,7,0, 0]
+		[3,3,3,10,10,8,8,8,1, 0]
+    	
+    	long arrayManipulation(int n, int[][] queries) {
+    	1<=a<=b<=n
+	 */
+	
+	public long arrayManipulation(int n, int[][] queries) {
+		if(n<3 && n>10000000) return 0;
+		int height = queries.length;
+		if( height<1 && height > (2*100000)) return 0;
+
+	    int[] controlArray = new int[n];
+	    int largest = 0;
+		for(int y=0;y<height;y++) {
+			if(queries[y][0]<1 && queries[y][1] <1)
+				return 0;
+			if(queries[y][0]> queries[y][1])
+				return 0;
+			for(int i=(queries[y][0])-1;i<(queries[y][1]);i++) {
+				controlArray[i]+=queries[y][2];
+				if(controlArray[i]>largest)
+					largest=controlArray[i];
+			}
+		}
+		return largest;
+	}
+	
+	public long arrayManipulation2(int n, int[][] queries) {
+		if(n<3 && n>10000000) return 0;
+		int height = queries.length;
+		if( height<1 && height > (2*100000)) return 0;
+
+	    long largest = findSumOfHighestOverlapOfNumberRanges(queries);
+		return largest;
+	}
+
+	public long findSumOfHighestOverlapOfNumberRanges(int[][] queries) {
+		
+		return 0;
+	}	
 
 }
--- SomeApp/test/com/alliconsulting/practice/tests/ArrayQueryAdderTests.java
+++ SomeApp/test/com/alliconsulting/practice/tests/ArrayQueryAdderTests.java
@@ -1,7 +1,6 @@
 package com.alliconsulting.practice.tests;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import com.alliconsulting.practice.app.ArrayQueryAdder;
@@ -12,7 +11,46 @@ class ArrayQueryAdderTests {
 	
 	@Test
 	void test() {
+		int arraySize = 10;
+		int[][] query = {
+				{1,5,3},
+				{4,8,7},
+				{6,9,1}
+				};		
 		
+		long actual = aqa.arrayManipulation(arraySize,query);
+		Assert.assertEquals(10, actual);
 	}
 
+	@Test
+	void test2() {
+		/*
+		 * 10 4
+			2 6 8
+			3 5 7
+			1 8 1
+			5 9 15
+			
+			
+		 1  2  3  4  5  6  7  8  9 10
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+		[0, 8, 8, 8, 8, 8, 0, 0, 0, 0]
+		[0, 8,15,15,15, 0, 0, 0, 0, 0]
+		[1, 9,16,16,16, 7, 1, 1, 0, 0]
+		[1, 9,16,16,31,22,16,16,15, 0]
+		 */
+
+		int arraySize = 10;
+		int[][] query = {
+				{2,6,8},
+				{3,5,7},
+				{1,8,1},
+				{5,9,15}
+				};		
+		
+		long actual = aqa.arrayManipulation(arraySize,query);
+		Assert.assertEquals(31, actual);		
+		
+	}
+	
 }
